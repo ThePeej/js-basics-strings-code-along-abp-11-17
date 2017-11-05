e8dd916a6e58982f83a76d2cc030f5e15cb57974
PJ Frias <peejfrias@gmail.com>
2017-11-05 22:33:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Peej';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,2)!}`
